5bd6a3d98e5737d94eff1075c8a1859963d6140a
KaashviSharma <87973727+KaashviSharma@users.noreply.github.com>
2022-05-09 16:55:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function setup()
+{
+    Canvas = createCanvas(400,400);
+    Canvas.position(580,250);
+    background("white");
+    Canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw()
+{
+    strokeWeight(8);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML  = 'Label : '+results[0].label;
+    document.getElementById('confidence').innerHTML = 'confidence : ' +Math.round(results[0].confidence *100)+'%';
+    utterThis  = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+
+}
+
+function clearCanvas()
+{
+    background("white");
+}
